5517c1338a156c072c3858d2a6467c5ce7a3282e
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2024-07-26 16:46:04 -0500

    first commit

---
+++ js/tailwindConfig.js
@@ -0,0 +1,31 @@
+//Configuración de colores generales
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        principalColor: "#1c92d2",
+      },
+      height: {
+        400: "400px",
+        cardsHeight: "270px",
+        containerServices: "1235px",
+        heightNews: "1412px",
+        heightFirstNew: "700px",
+        "95%": "95%",
+      },
+      width: {
+        cardsWidth: "280px",
+        400: "400px",
+        639: "639px",
+        1250: "1450px",
+        containerCards: "1280px",
+        1576: "1576px",
+        "95%": "95%",
+      },
+      spacing: {
+        "20px": "20px",
+      },
+    },
+  },
+};
